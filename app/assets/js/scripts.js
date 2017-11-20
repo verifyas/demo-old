15b3bb2f2aab132888ca4ed98382ac598557081b
Mahmoud Ilyan <mahmoudib@live.com>
2017-11-20 19:51:40 +0200

    Modal & Responsive First Version

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -13,6 +13,7 @@
   $(function () {
     // FastShell
     window.currentScreen = 1;
+    window.currentModalScreen = 1;
   });
 
   window.switchScreen = function(n) {
@@ -29,5 +30,29 @@
 
     return false; /* prevent form submissions */
   }
+
+
+  window.switchModalScreen = function(n) {
+    $('#modal-screen-' + currentModalScreen).fadeOut(function(){
+      $('#modal-screen-' + n).fadeIn();
+      currentModalScreen = n;
+    });
+
+
+
+    return false; /* prevent form submissions */
+  }
+
+
+  $("#close-modal").on("click" , function (){
+    $("#verify-modal").removeClass("is-active");
+    return false;
+  });
+
+  $("#verify-pay, #verify-pay-mobile").on("click", function (){
+    $("#verify-modal").addClass("is-active");
+    return false;
+  })
+
 
 })(jQuery, window, document);
